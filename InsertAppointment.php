7fa737006bfb9ede1648b8d9c35bdb7d04cd2213
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-11-01 23:56:14 +0600

    Update InsertAppointment.php

--- InsertAppointment.php
+++ InsertAppointment.php
@@ -74,7 +74,7 @@
         <tr style="height:200px;">
           <td><h1>Appointment Register</h1> <br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
             <a href="Dashboard.php">Dashboard</a> &nbsp &nbsp &nbsp 
-            <a href="">Patient</a>&nbsp &nbsp &nbsp
+            <a href="PatientList.php">Patient</a>&nbsp &nbsp &nbsp
             <a href="AppointmentView.php">Appointment</a>&nbsp &nbsp &nbsp
             <a href="Invoice.php">Transaction</a>&nbsp &nbsp &nbsp
             <a href="">Notification</a>&nbsp &nbsp &nbsp
