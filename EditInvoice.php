5985edde2c6879d99c74d28a76488bc8e2116682
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-11-04 20:49:55 +0600

    Update EditInvoice.php

--- EditInvoice.php
+++ EditInvoice.php
@@ -36,7 +36,7 @@
                 <tr style="height:200px;">
                     <td><h1>Edit Invoice</h1><br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
                         <a href="Dashboard.php">Dashboard</a> &nbsp &nbsp &nbsp 
-                        <a href="">Patient</a>&nbsp &nbsp &nbsp
+                        <a href="PatientList.php">Patient</a>&nbsp &nbsp &nbsp
                         <a href="AppointmentView.php">Appointment</a>&nbsp &nbsp &nbsp
                         <a href="Invoice.php">Transaction</a>&nbsp &nbsp &nbsp
                         <a href="">Notification</a>&nbsp &nbsp &nbsp
